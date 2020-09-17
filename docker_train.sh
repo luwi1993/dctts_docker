dd1d656e2055c32dfcf1dba48498e233d68f4eca
luwi1993 <lwidowski@alessio-analytics.com>
2020-09-17 21:53:12 +0200

    add docker_train.sh

---
+++ docker_train.sh
@@ -0,0 +1,1 @@
+sudo docker run -it -v "/home/lwidowski/Documents/sound/dctts_docker/logdir/:/dctts/logdir/:rw" -v "/home/lwidowski/Documents/sound/dctts_docker/results/:/dctts/samples/:rw" -v "/home/lwidowski/Documents/sound/dctts_docker/data:/dctts/data:rw" -v "/home/lwidowski/Documents/sound/dctts_docker/text_input:/dctts/text_input:rw" dctts
